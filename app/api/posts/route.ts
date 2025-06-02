fd7520bd79e355f46682da07b3457bc44e4c4abf
TAEEUN KIM <taeeunk1208@gmail.com>
2025-06-03 01:06:42 +0900

    fix minor error

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,7 +1,7 @@
 import { getPublishedPosts } from '@/lib/notion';
-import { NextResponse, type NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
 
-export async function GET(request: NextRequest) {
+export async function GET() {
   const posts = await getPublishedPosts();
 
   return NextResponse.json({ posts });
