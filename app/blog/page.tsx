b45a44c4f2a9c1b2603a747206bf8bfaed89e81a
TAEEUN KIM <taeeunk1208@gmail.com>
2025-06-08 22:22:01 +0900

    server actions practice

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,45 +1,5 @@
-import TagSection from '@/app/_components/TagSection';
-import ProfileSection from '@/app/_components/ProfileSection';
-import ContactSection from '@/app/_components/ContactSection';
-import { getPublishedPosts, getTags } from '@/lib/notion';
-import HeaderSection from '../_components/HeaderSection';
-import PostListSuspense from '@/components/features/blog/PostListSuspense';
-import PostListSkeleton from '@/components/features/blog/PostListSkeleton';
-import { Suspense } from 'react';
+import { redirect } from 'next/navigation';
 
-interface BlogProps {
-  searchParams: Promise<{ tag?: string; sort?: string }>;
-}
-
-export default async function Blog({ searchParams }: BlogProps) {
-  const { tag, sort } = await searchParams;
-  const selectedTag = tag || 'all';
-  const selectedSort = sort || 'latest';
-  const postsPromise = getPublishedPosts({ tag: selectedTag, sort: selectedSort });
-  const tags = await getTags();
-
-  return (
-    <div className="container py-8">
-      <div className="grid grid-cols-[200px_1fr_220px] gap-6">
-        {/* 좌측 사이드바 */}
-        <aside>
-          <TagSection tags={tags} selectedTag={selectedTag} />
-        </aside>
-        <div className="space-y-8">
-          {/* 섹션 제목 */}
-          <HeaderSection selectedTag={selectedTag} />
-
-          {/* 블로그 카드 그리드 */}
-          <Suspense fallback={<PostListSkeleton />}>
-            <PostListSuspense postsPromise={postsPromise} />
-          </Suspense>
-        </div>
-        {/* 우측 사이드바 */}
-        <aside className="flex flex-col gap-6">
-          <ProfileSection />
-          <ContactSection />
-        </aside>
-      </div>
-    </div>
-  );
+export default async function Blog() {
+  redirect('/');
 }
