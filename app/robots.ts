d9a19d08b38860e41d847b5c26e6004ab02fd3a1
TAEEUN KIM <taeeunk1208@gmail.com>
2025-06-10 16:26:15 +0900

    metadata files added

---
+++ app/robots.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: ['/api/'],
+    },
+    sitemap: 'https://notion-blog-six-lyart.vercel.app/sitemap.xml',
+  };
+}
